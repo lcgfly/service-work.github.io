e67a7f850cb9e64e42f9ce1cf5d29bb6737738f2
林英杰 <linyingjie@fcbox.com>
2021-01-21 10:17:25 +0800

    修改文件路径

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
   window.onload=function(){
-    navigator.serviceWorker.register('./sw.js').then(res=>{
+    navigator.serviceWorker.register('/service-work.github.io/sw.js').then(res=>{
       console.warn('service-worker is ready' + res.scope)
     }).catch(e=>console.warn(e))
   }
